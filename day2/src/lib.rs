8e8b5b536014f191a7f7d824c62e0bb8cd852a4e
Frank Wang <frank@circleci.com>
2019-12-27 11:46:35 -0500

    Upgrade intcode computer in day2 code

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,29 +1,28 @@
 use intcode_computer as intcode;
 
-pub fn pt1(path_to_input: &str) -> Result<usize, String> {
-    let mut intcode = intcode::parse_intcode(&intcode::read_file(path_to_input));
-    intcode::process_inputs(12, 2, &mut intcode)
+pub fn pt1(path_to_input: &str) -> Result<i32, String> {
+    let mut computer = intcode::IntcodeComputer::new(&intcode::read_file(path_to_input), Vec::new());
+    intcode::process_inputs(12, 2, &mut computer)
 }
 
-fn try_input(noun: usize, verb: usize, intcode: &Vec<usize>, required_output: usize) -> bool {
-    let mut intcode = intcode.clone();
-    match intcode::process_inputs(noun, verb, &mut intcode) {
+fn try_input(noun: i32, verb: i32, computer: &intcode::IntcodeComputer, required_output: i32) -> bool {
+    let mut computer = computer.clone();
+    match intcode::process_inputs(noun, verb, &mut computer) {
         Ok(val) => val == required_output,
         _ => false,
     }
 }
 
-pub fn pt2(path_to_input: &str, required_output: usize) -> Option<(usize, usize)> {
-    let intcode = intcode::parse_intcode(&intcode::read_file(path_to_input));
-
+pub fn pt2(path_to_input: &str, required_output: i32) -> Option<(i32, i32)> {
+    let computer = intcode::IntcodeComputer::new(&intcode::read_file(path_to_input), Vec::new());
     for i in 0..1000 {
         for j in 0..1000 {
-            if try_input(i, j, &intcode, required_output) {
+            if try_input(i, j, &computer, required_output) {
                 return Some((i, j));
             }
         }
     }
-    return None;
+    None
 }
 
 #[cfg(test)]
@@ -31,11 +30,11 @@ mod tests {
     use super::*;
     #[test]
     fn pt1_test() {
-        assert_eq!(pt1("input"), Ok(3101844));
+        assert_eq!(pt1("input"), Ok(3_101_844));
     }
 
     #[test]
     fn pt2_test() {
-        assert_eq!(pt2("input", 19690720), Some((84, 78)));
+        assert_eq!(pt2("input", 19_690_720), Some((84, 78)));
     }
 }
