e334ed5de324bb71a1f524302255b201081872f2
Frank Wang <frank@circleci.com>
2019-12-28 17:07:15 -0500

    Add day5 part 2

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,6 +1,6 @@
 use intcode_computer::*;
 
-fn pt1 (path_to_input: &str) -> Result<i32, String> {
+fn pt1(path_to_input: &str) -> Result<i32, String> {
     let raw_code = read_file(path_to_input);
     let mut computer = IntcodeComputer::new(&raw_code, vec![1]);
     computer.run()?;
@@ -17,11 +17,30 @@ fn pt1 (path_to_input: &str) -> Result<i32, String> {
     }
 }
 
+fn pt2(path_to_input: &str) -> Result<i32, String> {
+    let raw_code = read_file(path_to_input);
+    let mut computer = IntcodeComputer::new(&raw_code, vec![5]);
+    computer.run()?;
+    if computer.output.len() != 1 {
+        Err(format!(
+            "Expected a single output. Got {} outputs instead.",
+            computer.output.len()
+        ))
+    } else {
+        Ok(computer.output[0])
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn it_works() {
+    fn pt1_works() {
         assert_eq!(pt1("input").unwrap(), 13_547_311);
+    }
+
+    #[test]
+    fn pt2_works() {
+        assert_eq!(pt2("input").unwrap(), 236_453);
     }
 }
